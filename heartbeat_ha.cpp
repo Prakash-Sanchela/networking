52ba36cc365f3f92974ac4a1d13db8a3c796583f
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-07-20 01:02:26 +0530

    Update heartbeat_ha.cpp

--- heartbeat_ha.cpp
+++ heartbeat_ha.cpp
@@ -1,1 +1,6 @@
 // write a program to check the heart beat monitoring system in load balancer or HA
+# keywords
+# keepalived
+# vrrp 
+# how are you achieve seamless traffic when failover happens -> duplicating sessions to passive node
+# HA states

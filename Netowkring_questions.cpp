a4db2bf2f14041ce0cf3fad830c06ea11930c96a
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-09-29 11:43:38 +0530

    Create Netowkring_questions.cpp

---
+++ Netowkring_questions.cpp
@@ -0,0 +1,18 @@
+How Packet processing has been done in typical Linux machine
+  egress traffic 
+  ingress traffic
+OSI Reference model
+Draw packet at each layer
+IP subnetting 
+VLAN vs subnetting 
+How DHCP & DNS works
+  Check packets in wireshark
+How IP routing has been done when you ping google.com
+  - Packet at each layer. 
+How Routing has been done
+  - Check packet level what changes router needs to be done
+  - Routing protocol
+      - Link state 
+      - distance vector
+Routing and switching 
+HA scenario
